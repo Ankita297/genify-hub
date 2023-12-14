793f4b2eb27b2bc6558390ae5eb734a83c814d8d
Ankita-netix <ankita@netix.ai>
2023-12-14 13:33:12 +0530

    integrated clerk-authentication

---
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,10 @@
+import { UserButton } from "@clerk/nextjs";
+
+export default function DashboardPage() {
+  return (
+    <div>
+      <h1>This is the Dashboard Page</h1>;
+      <UserButton afterSignOutUrl="/" />
+    </div>
+  );
+}
